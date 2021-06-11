ce80300f04435c5c1b32faa0e03c66d96b8629a7
VladimirMoor <81152453+VladimirMoor@users.noreply.github.com>
2021-06-11 13:28:46 +0300

    Initial Commit

---
+++ HobbyTracker/HobbyTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HobbyTrackerApp.swift
+//  HobbyTracker
+//
+//  Created by Vladimir on 11.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct HobbyTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
